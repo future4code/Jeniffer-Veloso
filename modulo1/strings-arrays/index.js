621d0733122963b3cacb01266d6a8cc9a937b271
jeni-veloso <jeniveloso22@gmail.com>
2021-10-06 17:54:53 -0300

    Resolução dos exercícios

---
+++ modulo1/strings-arrays/index.js
@@ -0,0 +1,42 @@
+/* RESPOSTA EXERCÍCIOS DE INTERPRETAÇÃO
+1-
+a.  undefined
+ b.  null
+ c.  11
+ d.  3
+ e.  (11) [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+ 2 - SUBI NUM ONIBUS EM MIRROCOS  27
+
+ RESPOSTA EXERCÍCIO ESCRITA DE CÓDIGO
+
+ 1-const nomeDoUsuario = prompt (" Qual é seu nome?")
+const emailDoUsuario = prompt ("Qual é seu e-mail?")
+const fraseConcatenacao = " O e-mail "  +  emailDoUsuario + " foi cadastrado com sucesso. Seja bem vindo(a) " + nomeDoUsuario + " ! "
+console.log (fraseConcatenacao)
+
+2 - a) const comidasPreferidas = [" Sushi ", " Churrasco ", " Açaí ", " Macarrão ", " Camarão "]
+console.log (comidasPreferidas)
+    b)const comidasPreferidas = [" Sushi ", " Churrasco ", " Açaí ", " Macarrão ", " Camarão "]
+
+     console.log (" Essas são minhas comidas preferidas ", comidasPreferidas)
+
+
+3 - const lista De Tarefas = []
+    const listasDeTarefas =  prompt (" Quais as tres tarefas que você precisa realizar no dia?")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
